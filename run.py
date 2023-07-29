4ed02289a5c7c606748fab8bc10d1f6e1efd0ba4
annbueno <138026558+annbueno@users.noreply.github.com>
2023-07-29 22:44:14 +0800

    Update run.py

--- run.py
+++ run.py
@@ -15,17 +15,13 @@ myapp = Flask(__name__)
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-parser = WebhookParser(channel_secret)
+handler = WebhookHandler(channel_secret)
 
 
 @myapp.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
