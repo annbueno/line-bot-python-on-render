e5945e6d17a7da0ad577a50e96a209639130db4c
annbueno <138026558+annbueno@users.noreply.github.com>
2023-07-29 22:12:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,72 +1,32 @@
+import os
+import sys
 from flask import Flask, request, abort
-
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi, WebhookParser
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
-
-import os
-import openai
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 myapp = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
 
-def gpt_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
-    answer = response['choices'][0]['text'].replace('。', '')
-    return answer
-
-
-# 監聽所有來自 /callback 的 Post Request
 @myapp.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    # handle webhook body
     try:
-        handler.handle(body, signature)
+        # 網址被執行時，等同使用 GET 方法發送 request，觸發 LINE Message API 的 push_message 方法
+        line_bot_api.push_message('U0bcbd8d8784be8615a919ddceb0d0b28', TextSendMessage(text='...0960'))
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    gpt_answer = gpt_response(msg)
-    print(gpt_answer)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(gpt_answer))
-
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == '__main__':
