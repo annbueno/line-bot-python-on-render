9fffe8a2efc920b9dc15403a64faf791bf8a1fd1
annbueno <138026558+annbueno@users.noreply.github.com>
2023-07-29 23:36:35 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookParser, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage
+    MessageEvent, TextMessage, TextSendMessage
 )
+
+from getexceldata import output_excel_data
 
 myapp = Flask(__name__)
 
@@ -17,9 +19,12 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+output_str = output_excel_data('陳沱')
+
 
 @myapp.route("/callback", methods=['POST'])
 def callback():
+    line_bot_api.push_message('C913bcb87db489f8af1dc7b392a303e73', TextSendMessage(text=output_str))
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     try:
